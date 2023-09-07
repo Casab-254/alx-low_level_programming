94fbc6805484db020e3f80f79c12f236e1659b83
Casab-254 <afrikiddoh@gmail.com>
2023-09-07 15:35:05 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -contains output using puts
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	puts("\n"Programming is like building a multilingual puzzle\n");
+		return (0);
+}

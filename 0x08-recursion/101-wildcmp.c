bdbb78c5745b916b46b23df722539d2ef1eb3f8c
Casab-254 <afrikiddoh@gmail.com>
2024-01-10 15:34:02 -0800

    q1 correction plus question 2

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * wildcmp - Compares two strings considering the special character '*'.
+ * * wildcmp_helper - Helper function for wildcmp, handles non-'*' characters.
+ * @s1: The first string.
+ * @s2: The second string, which can contain '*'.
+ * @c:  is a parameter for the _putchar function.
+ *
+ * Return: 1 if strings are identical, otherwise 0.
+ */
+int _putchar(char c);
+int wildcmp_helper(char *s1, char *s2);
+
+int wildcmp(char *s1, char *s2);
+{
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0')
+			return (1);
+		return (wildcmp_helper(s1, s2 + 1) || wildcmp_helper(s1 + 1, s2));
+	}
+
+	return (wildcmp_helper(s1, s2));
+}
+
+int wildcmp_helper(char *s1, char *s2);
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
+

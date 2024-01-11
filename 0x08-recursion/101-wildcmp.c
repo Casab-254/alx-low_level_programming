86592c9b4621394ef44c5fdb913cf4ce35a1ebe0
Casab-254 <afrikiddoh@gmail.com>
2024-01-10 16:02:07 -0800

    while using the while

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,15 +1,12 @@
 #include "main.h"
-int _putchar(char c);
-
-int wildcmp_helper(char *s1, char *s2);
 
 /**
  * wildcmp - Compares two strings considering the special character '*'.
  * @s1: The first string.
  * @s2: The second string, which can contain '*'.
- * * wildcmp_helper - Helper function for wildcmp, handles non-'*' characters.
- * @c: returns the parameters within which the function operates;
  *
+ * wildcmp_helper - Helper function for wildcmp, handles non-'*' characters.
+ * @c: returns the function parameters.
  * Return: 1 if strings are identical, otherwise 0.
  */
 int _putchar(char c);
@@ -20,20 +17,27 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '*')
 	{
-		if (*(s2 + 1) == '\0')
-		       	return 1;
-		return wildcmp_helper(s1, s2 + 1) || wildcmp_helper(s1 + 1, s2);
+		while (*s2 == '*')
+			s2++;
+
+		if (*s2 == '\0')
+			return 1;
+
+		while (*s1 != '\0' && *s1 != *s2)
+			s1++;
+		return (*s1 != '\0') && wildcmp(s1, s2);
 	}
-	
+
 	return wildcmp_helper(s1, s2);
 }
 
-int wildcmp_helper(char *s1, char *s2) {
+int wildcmp_helper(char *s1, char *s2)
+{
 	if (*s1 == '\0' && *s2 == '\0')
 		return 1;
 
 	if (*s1 == *s2)
 		return wildcmp(s1 + 1, s2 + 1);
-
+	
 	return 0;
 }
